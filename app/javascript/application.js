bccc78e0b79a5f72f10ba02440a07005c1780049
Hamza <hamzadev140@gmail.com>
2024-03-08 12:55:12 +0500

    fix confirmation message not visible

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+$(document).on("click", "[data-confirm]", function (e) {
+    return confirm($(this).data("confirm"));
+});
